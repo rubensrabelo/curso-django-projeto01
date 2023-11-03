11e6be46ca8f3f1f7174f14d65d4c41e9c3b47b9
Rubens Rabelo <rubensrabelo123@gmail.com>
2023-11-03 10:32:13 -0300

    Removendo os dados de sobre e contato

--- recipes/views.py
+++ recipes/views.py
@@ -3,12 +3,4 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'global/home.html')
-
-
-def sobre(request):
-    return HttpResponse('SOBRE 1')
-
-
-def contato(request):
-    return HttpResponse('CONTATO 1')
+    return render(request, 'recipes/home.html')
